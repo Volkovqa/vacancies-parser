20eeae8b946ef74a0b86acf7e88516b8b55dc89c
Egor Volkov <iam.volkov@yandex.ru>
2023-05-28 14:01:48 +0300

    Обработка разных сценариев пользования программой в функции main.

--- main.py
+++ main.py
@@ -4,45 +4,71 @@ from src.utils import *
 
 def main():
 
+    print("Привет. "
+          "Эта консольная программа поможет Вам выгрузить и отсортировать интересующие Вас вакансии с платформ"
+          "HeadHunter и SuperJob")
+
+    while True:
+        choose_platform = input("Введите команду, с каких платформ вы хотите выгрузить вакансии или exit для выхода:\n"
+                                "HH - HeadHunter\n"
+                                "SJ - SuperJob\n"
+                                "HS - HeadHunter и SuperJob\n"
+                                "exit - выход из программы\n")
+
+        if choose_platform == "exit":
+            exit()
+
+        elif choose_platform.lower() in ("hh", "sj", "hs"):
+            break
+
+        else:
+            print("Неправильная команда")
+
     keyword = input("Введите ключевое слово для поиска вакансий: ")
 
-    hh_engine = HHru(keyword)
-    sj_engine = SuperJob(keyword)
+    all_vacancies = []
+    all_savers = []
 
-    hh_saver = hh_engine.get_json_saver("parsed_data/hh_vacancies.json")
-    sj_saver = sj_engine.get_json_saver("parsed_data/sj_vacancies.json")
+    if choose_platform.lower() in ("hh", "hs"):
 
-    for page in range(1):
+        hh_engine = HHru(keyword)
+        hh_saver = hh_engine.get_json_saver("parsed_data/hh_vacancies.json")
 
-        hh_vacancies = hh_engine.get_request().json()["items"]
-        for vacancy in hh_vacancies:
-            hh_saver.insert(vacancy)
+        for page in range(1):
 
-        sj_vacancies = sj_engine.get_request().json()["objects"]
-        for vacancy in sj_vacancies:
-            sj_saver.insert(vacancy)
+            hh_vacancies = hh_engine.get_request().json()["items"]
+            for vacancy in hh_vacancies:
+                hh_saver.insert(vacancy)
+
+        all_savers.append(hh_saver)
+        hh_vacancies = get_hh_vacancies_list(hh_saver)
+        all_vacancies += hh_vacancies
+
+    if choose_platform.lower() in ("sj", "hs"):
+        sj_engine = SuperJob(keyword)
+        sj_saver = sj_engine.get_json_saver("parsed_data/sj_vacancies.json")
+
+        for page in range(1):
+
+            sj_vacancies = sj_engine.get_request().json()["objects"]
+            for vacancy in sj_vacancies:
+                sj_saver.insert(vacancy)
+
+        all_savers.append(sj_saver)
+        sj_vacancies = get_sj_vacancies_list(sj_saver)
+        all_vacancies += sj_vacancies
 
     while True:
 
         command = input("Введите команду (sort или top): ")
 
         if command == "sort":
-
-            hh_vacancies = get_hh_vacancies_list(hh_saver)
-            sj_vacancies = get_sj_vacancies_list(sj_saver)
-
-            sorted_vacancies = sorting(hh_vacancies + sj_vacancies)
+            sorted_vacancies = sorting(all_vacancies)
 
             for vacancy in sorted_vacancies:
                 print(vacancy)
 
         elif command == "top":
-
-            hh_vacancies = get_hh_vacancies_list(hh_saver)
-            sj_vacancies = get_sj_vacancies_list(sj_saver)
-
-            all_vacancies = hh_vacancies + sj_vacancies
-
             top_count = int(input("Введите количество вакансий для вывода: "))
 
             top_vacancies = get_top(all_vacancies, top_count)
@@ -56,8 +82,8 @@ def main():
 
         if continue_running.lower() == "n":
 
-            hh_saver.clear_data()
-            sj_saver.clear_data()
+            for saver in all_savers:
+                saver.clear_data()
 
             break
 
